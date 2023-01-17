2356060401ced3fbf83f5d6f20254e2b7d619646
Vanágila Xavier Rodrigues <91071541+vanagila@users.noreply.github.com>
2023-01-17 15:31:58 -0300

    Develop (#12)
    * folders
    
    * feat: input search
    
    * feat: add page title and aclear button
    
    * feat: add gifs container
    
    * feat: add feedback for users
    
    * feat: add search gif using giphy api
    
    * feat: render a random  gif
    
    * feat: creates a video element
    
    * feat: render the search on screen
    
    * feat: render feedback to users
    
    * feat: clear input search and reset
    
    * feat: makes the app responsive

---
+++ js/script.js
@@ -0,0 +1,120 @@
+const API_KEY = "IuSylkr4O49mCDh0RFRv1cbvjql5PxNi";
+const searchInput = document.querySelector(".header__input");
+const searchBtn = document.querySelector(".header__btn");
+const clearBtn = document.querySelector(".clear__btn");
+const gifsContainer = document.querySelector(".gifs__container");
+const hint = document.querySelector(".feedback__hint");
+
+const randomGif = (arr) => {
+  const random = Math.floor(Math.random() * arr.length);
+
+  return arr[random];
+};
+
+const createVideo = (src) => {
+  const video = document.createElement("video");
+
+  video.src = src;
+  video.autoplay = true;
+  video.muted = "muted";
+  video.loop = true;
+  video.className = "video";
+
+  return video;
+};
+
+const toggleLoading = (state) => {
+  if (state) {
+    document.body.classList.add("loading");
+    searchInput.disable = true;
+  } else {
+    document.body.classList.remove("loading");
+    searchInput.disable = false;
+    searchInput.focus();
+  }
+};
+
+const searchGiphy = (searchTerm) => {
+  toggleLoading(true);
+
+  fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      const gif = randomGif(json.data);
+      const src = gif.images.original.mp4;
+      const video = createVideo(src);
+
+      gifsContainer.append(video);
+
+      video.addEventListener("loadeddata", () => {
+        video.classList.add("visible");
+
+        toggleLoading(false);
+
+        document.body.classList.add("has-results");
+
+        hint.innerHTML = `Hit enter or click on <i class="fa-solid fa-magnifying-glass header__btn-img"></i>  to search more ${searchTerm}`;
+      });
+    })
+    .catch(() => {
+      toggleLoading(false);
+
+      hint.innerText = `Nothing found for ${searchTerm}`;
+    });
+};
+
+const doSearch = () => {
+  const searchTerm = searchInput.value;
+
+  if (searchTerm.length > 2) {
+    hint.classList.add("show-hint");
+    hint.innerHTML = `Hit enter or click on <i class="fa-solid fa-magnifying-glass header__btn-img"></i> to search ${searchTerm}`;
+
+    searchGiphy(searchTerm);
+  } else {
+    hint.classList.remove("show-hint");
+  }
+};
+
+searchBtn.addEventListener("click", doSearch);
+document.addEventListener("keyup", (ev) => {
+  const searchTerm = searchInput.value;
+
+  if (searchTerm.length > 2) {
+    hint.classList.add("show-hint");
+    hint.innerHTML = `Hit enter or click on <i class="fa-solid fa-magnifying-glass header__btn-img"></i> to search ${searchTerm}`;
+  } else {
+    hint.classList.remove("show-hint");
+  }
+
+  if (ev.key === "Enter" && searchTerm.length > 2) {
+    searchGiphy(searchTerm);
+  }
+});
+
+const clearSearch = () => {
+  document.body.classList.remove("has-results");
+
+  gifsContainer.innerHTML = `<video
+  class="first-gif full-area"
+  src="https://media2.giphy.com/media/CzbiCJTYOzHTW/giphy.mp4?cid=bcfe5c67gjsb1lq61zjthpej774r5mmme4xchaieuswseiyd&rid=giphy.mp4&ct=g"
+  autoplay="true"
+  loop="true"
+  class="video"
+  muted="muted"
+></video>`;
+  searchInput.value = "";
+  hint.innerText = "";
+  searchInput.focus();
+};
+
+clearBtn.addEventListener("click", clearSearch);
+document.addEventListener("keyup", (ev) => {
+  if (ev.key === "Escape") {
+    clearSearch();
+  }
+});
